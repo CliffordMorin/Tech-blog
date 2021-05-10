cf1550511bce60c5809102e549557a28b3440de4
Clifford Morin <cemorin21@gmail.com>
2021-05-10 15:30:19 -0400

    updated handlebars

--- models/Post.js
+++ models/Post.js
@@ -19,6 +19,11 @@ Post.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
