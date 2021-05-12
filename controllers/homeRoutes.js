49a6b981d08ec74b41e29a111af3e89419dfd216
Clifford Morin <cemorin21@gmail.com>
2021-05-12 00:53:09 -0400

    added css and fixed thandle bar templates

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment} = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
